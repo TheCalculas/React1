18b5e964ab2e322ea66eeaadad2a06db677b4280
TheCalculas <77923808+TheCalculas@users.noreply.github.com>
2022-02-26 10:26:41 +0530

    hey

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
+import React, { useState, useEffect } from "react";
+import NavBar from "./NavBar";
 function App() {
+  const [count, setCount] = useState(0);
+  const [myStyle, setMyStyle] = useState({
+    color: "black",
+    backgroundColor: "white",
+  });
+  const [btnText, SetBtnText] = useState("Enable Dark mode");
+  const toogleStyle = () => {
+    if (myStyle.color === "black") {
+      setMyStyle({
+        color: "white",
+        backgroundColor: "black",
+        border: "1px solid white",
+      });
+      SetBtnText("Enable Light Mode");
+    } else {
+      setMyStyle({
+        color: "black",
+        backgroundColor: "white",
+      });
+      SetBtnText("Enable Dark Mode");
+    }
+  };
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `You clicked ${count} times`;
+  });
+  // setCount(5);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      <div style={myStyle} class="container">
+        <p>You clicked {count} times</p>
+        <button onClick={() => setCount(count + 1)}>Click me</button>
+        <div className="container center">
+          <button
+            onClick={toogleStyle}
+            type="button"
+            className="btn btn-primary"
+          >
+            {" "}
+            {btnText}
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
 
